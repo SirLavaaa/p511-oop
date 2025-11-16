348d13c54c7571de23f929c617e0e4c283cd1b25
SirLavaaa <lava12597@gmail.com>
2025-11-16 13:23:18 +0500

    добавил класс Studen

--- Program.cs
+++ Program.cs
@@ -9,31 +9,36 @@ using System.Web;
 namespace p511_oop
 {
 
-    class User
+    class Student
     {
-        public string name;
-        public string surename;
+        public string firstName;
+        public string lastName;
         public int age;
-        public string city;
+        
 
-        public User(string name, string surename, int age, string city) 
+        public Student(string firstName, string lastName, int age) 
         { 
-          this.name = name;
-          this.surename = surename;
+          this.firstName = firstName;
+          this.lastName = lastName;    
           this.age = age;
-          this.city = city;
         }
-        public void getVaules()
+        public void Introduce()
         {
-            Console.WriteLine($"name {name}, age {age}, city {city}, surename {surename}");
+            Console.WriteLine($"Привет, я {firstName} {lastName}, мне {age}");
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            User user = new User("Alex", "Morgan", 17, "New York");
-            user.getVaules();
+            Student studentOne = new Student("Пётр" ,"Петров" , 19);
+            Student studentTwo = new Student("Илья" ,"Сидоров" , 21);
+            Student studentThree = new Student("Иван" ,"Иванов" , 17);
+            studentOne.Introduce();
+            studentTwo.Introduce();
+            studentThree.Introduce();
+
+
         }
     }
 }
