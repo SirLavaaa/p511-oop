0a3c74cb05dd2db3ab5740e3f561437ba21f3deb
SirLavaaa <lava12597@gmail.com>
2025-11-16 14:04:00 +0500

    Добаивил конструктор Rectangle и вывод ошибки когда какое-либо из значений меньше 0

--- Program.cs
+++ Program.cs
@@ -8,8 +8,52 @@ namespace p511_oop
 {
     public class Rectangle
     {
-        public double width;
-        public double height;
+        private double width;
+        private double height;
+        
+        public Rectangle(double widht , double height)
+        {
+            Width = widht;
+            Height = height;
+        }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка, какое-то значение меньше 0");
+                }
+            }
+        }
+        public double Height
+        {
+            get
+            { 
+                return height; 
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка, какое-то значение меньше 0");
+                }
+            }
+        }   
+
+
         public double GetArea()
         {
             return width * height;
@@ -18,22 +62,18 @@ namespace p511_oop
         {
             return 2 * (width + height);
         }
-        public void SetVaules(double _width, double _height)
-        {
-            width = _width;
-            height = _height;
-        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Rectangle rect = new Rectangle();
-            rect.SetVaules(16.3, 12);
-            double area = rect.GetArea();
-            Console.WriteLine(area);
-            double perim = rect.GetPerimeter();
-            Console.WriteLine(perim);
+            Rectangle rect1 = new Rectangle(17.2 , 12.6);
+            Console.WriteLine(rect1.GetArea());
+            Console.WriteLine(rect1.GetPerimeter());
+            
+            Rectangle rect2 = new Rectangle(-11, 12);
+            Console.WriteLine($"Widt rect2 = {rect2.Width}");
+            Console.WriteLine($"Height rect2 = {rect2.Height}");
         }
     }
 }
