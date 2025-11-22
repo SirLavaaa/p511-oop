9520ee26f581aa8f90c8ecdaaa33539d5d2ab8ee
SirLavaaa <lava12597@gmail.com>
2025-11-22 14:58:03 +0500

    сделал метод  вычисляющий факториал

--- Program.cs
+++ Program.cs
@@ -8,37 +8,21 @@ using System.Web;
 
 namespace p511_oop
 {
-
-    class Student
-    {
-        public string firstName;
-        public string lastName;
-        public int age;
-        
-
-        public Student(string firstName, string lastName, int age) 
-        { 
-          this.firstName = firstName;
-          this.lastName = lastName;    
-          this.age = age;
-        }
-        public void Introduce()
-        {
-            Console.WriteLine($"Привет, я {firstName} {lastName}, мне {age}");
-        }
-    }
     internal class Program
     {
+        static int Factorial(int n)
+        {
+            int b = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                b *= i;
+            }
+            return b;
+        }
         static void Main(string[] args)
         {
-            Student studentOne = new Student("Пётр" ,"Петров" , 19);
-            Student studentTwo = new Student("Илья" ,"Сидоров" , 21);
-            Student studentThree = new Student("Иван" ,"Иванов" , 17);
-            studentOne.Introduce();
-            studentTwo.Introduce();
-            studentThree.Introduce();
-
-
+            int number = 5;
+            Console.WriteLine(Factorial(number));
         }
     }
 }
