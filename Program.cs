c6417f88f2a02b60414f1902f96b31cb6c03701f
SirLavaaa <lava12597@gmail.com>
2025-11-23 13:09:34 +0500

    очистил ветку Master

--- Program.cs
+++ Program.cs
@@ -8,37 +8,11 @@ using System.Web;
 
 namespace p511_oop
 {
-
-    class Student
-    {
-        public string firstName;
-        public string lastName;
-        public int age;
-        
-
-        public Student(string firstName, string lastName, int age) 
-        { 
-          this.firstName = firstName;
-          this.lastName = lastName;    
-          this.age = age;
-        }
-        public void Introduce()
-        {
-            Console.WriteLine($"Привет, я {firstName} {lastName}, мне {age}");
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Student studentOne = new Student("Пётр" ,"Петров" , 19);
-            Student studentTwo = new Student("Илья" ,"Сидоров" , 21);
-            Student studentThree = new Student("Иван" ,"Иванов" , 17);
-            studentOne.Introduce();
-            studentTwo.Introduce();
-            studentThree.Introduce();
-
-
+            
         }
     }
 }
