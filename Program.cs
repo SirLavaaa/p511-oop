88b2b015a787189fc7ef97fe4eadd47e549fb98a
CYBER ARTEL <CYBER ARTEL@DESKTOP-AQOEL74>
2025-12-09 15:16:32 +0300

    сделал класс Inventory

--- Program.cs
+++ Program.cs
@@ -11,8 +11,18 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-           Phone phone = new Phone("Samsumg" , "S23" , 50000);
-            phone.Print();
+            List<Inventory> inventories = new List<Inventory>()
+            {
+                new Inventory(1, "rice", 10),
+                new Inventory(2, "bread", 30)
+            };
+            Console.WriteLine("введите id товара");
+            int id = Convert.ToInt32(Console.ReadLine());
+            foreach (Inventory item in inventories)
+            {
+                if(item._id == id)
+                   item.Print();
+            }
         }
     }
 }
