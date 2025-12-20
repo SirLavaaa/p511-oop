e26937addd55aa55977705e8bf190356eca65eca
SirLavaaa <lava12597@gmail.com>
2025-12-20 13:55:21 +0500

    поработал с классом Stream

--- Program.cs
+++ Program.cs
@@ -12,9 +12,65 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            File.AppendAllText(@"C:\Users\Student\Desktop\hi.txt", "\nbye");
-            string text = File.ReadAllText(@"C:\Users\Student\Desktop\hi.txt");
-            Console.WriteLine(text);
+            StreamWriter streamWriter = null;
+            StreamReader streamReader = null;
+            try
+            {
+                streamWriter = new StreamWriter(
+                      @"C:\Users\Student\Desktop\hi.txt",
+                      append: false,
+                      encoding: Encoding.UTF8
+                    );
+                streamWriter.WriteLine("Bye");
+                Console.WriteLine("Данные успешно записаны");
+            }
+            catch (UnauthorizedAccessException ex)
+            { 
+              Console.WriteLine(ex.Message);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("неизвестная ошибка");
+            }
+            finally
+            {
+                if (streamWriter != null)
+                {
+                    streamWriter.Close();
+                }
+            }
+            try
+            {
+                streamReader = new StreamReader(
+                      @"C:\Users\Student\Desktop\hi.txt",
+                      encoding: Encoding.UTF8
+                    );
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("неизвестная ошибка");
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+            }
+
         }
     }
 }
