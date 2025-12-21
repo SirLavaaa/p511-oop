d75c258cc4ab9d8b5e58b1dc66503adb6648923b
SirLavaaa <lava12597@gmail.com>
2025-12-21 14:15:12 +0500

    добавил меню

--- Program.cs
+++ Program.cs
@@ -16,9 +16,24 @@ namespace p511_oop
             students.Add(new Student("Dave", 21, 4.2));
             students.Add(new Student("Jhon", 19, 5.0));
             students.Add(new Student("Mike", 21, 3.2));
-            students.Print();
-            students.SaveToFile(@"C:\Users\Student\Desktop\Student.txt");
-            students.LoadFromFile(@"C:\Users\Student\Desktop\Student.txt");
+            //students.Print();
+            //students.SaveToFile(@"C:\Users\Student\Desktop\Student.txt");
+            //students.LoadFromFile(@"C:\Users\Student\Desktop\Student.txt");
+            Console.WriteLine("Что вы хотите сделать? \n1-добавить студент \n2-вывести на экран \n3-сохранить что-то \n4-загрузить файл");
+            int draft = Convert.ToInt32(Console.ReadLine());
+            if (draft == 1)
+                students.Add(new Student(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())));
+            else if (draft == 2)
+                students.Print();
+            else if (draft == 3)
+                students.SaveToFile(Console.ReadLine());
+            else if (draft == 4)
+                students.LoadFromFile(Console.ReadLine());
+            else if (draft == 0)
+                Console.WriteLine("Program End");
+            else
+                Console.WriteLine("Error");
         }
+        
     }
 }
