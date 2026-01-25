3374c8c97601ee590ec98a9983fc442da1526712
SirLavaaa <lava12597@gmail.com>
2026-01-25 14:19:35 +0500

    получил данные с локального сервера

--- Program.cs
+++ Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace p511_oop
 {
+    internal class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient _httpClient = new HttpClient()
         {
-           User user = new User();
-            user.setName("Jhon");
-            user.setAge(18);
-            Console.WriteLine(user.getName());
-            Console.WriteLine(user.getAge());
+            BaseAddress = new Uri("http://localhost:8080")
+        };
+        static async Task Main(string[] args)
+        {
+            List<UserDto> users = await _httpClient.GetFromJsonAsync<List<UserDto>>("/api/users");
+            if (users is null)
+            {
+                Console.WriteLine("null");
+            }
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Username);
+            }
         }
     }
 }
